fd6b1e500a2f6884944c4e8f953edf9c1081694a
DavisHenckel <Davishenckel@hotmail.com>
2023-04-21 09:45:36 -0700

    Add option for OK Button Text

--- Public/InformationalPopup.ps1
+++ Public/InformationalPopup.ps1
@@ -7,11 +7,13 @@ function InformationalPopup {
         [parameter(Mandatory=$false)]
         [int32]$Width = 325,
         [parameter(Mandatory=$false)]
-        [int32]$Height = 200
+        [int32]$Height = 200,
+        [parameter(Mandatory=$false)]
+        [string]$OKButtonText = "OK"
 
     )
     #Below line loads the XML file defining the GUI, and loads the corresponding variables into the XML
-    [XML]$Form = (Get-Content -Path ($PSScriptRoot + "\..\XAML\InformationalGUI.xml")).Replace("`$Title",$Title).Replace("`$Content",$Message).Replace("`$Height",$Height).Replace("`$Width",$Width)
+    [XML]$Form = (Get-Content -Path ($PSScriptRoot + "\..\XAML\InformationalGUI.xml")).Replace("`$OKButtonText", $OKButtonText).Replace("`$Title",$Title).Replace("`$Content",$Message).Replace("`$Height",$Height).Replace("`$Width",$Width)
     $NodeReader = (New-Object System.Xml.XmlNodeReader $Form)
     $Window = [Windows.Markup.XamlReader]::Load($NodeReader)
     $Button = $Window.FindName("OkButton")
